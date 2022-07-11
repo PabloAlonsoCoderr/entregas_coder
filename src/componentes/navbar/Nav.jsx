b3c63d0933cb0b57d8c1b4367af00c1ecf3c3133
PabloCoderr <gestionexxo+pepe@gmail.com>
2022-07-10 22:20:18 -0200

    entrega_dos

---
+++ src/componentes/navbar/Nav.jsx
@@ -0,0 +1,47 @@
+import "./Nav.css";
+import logo_nav from "../../assets/img/logo_Nav.png";
+function Nav() {
+  return (
+    <div className="wrapper">
+      <div className="navbar">
+        <div className="navbar__section--1">
+          <img src={logo_nav} className="navbar__logo" />
+        </div>
+
+        <div className="navbar__section--2">
+          <ul class="navbar__ul">
+            <li class="navbar__li">
+              <a class="navbar__a" href="#">
+                INICIO
+              </a>
+            </li>
+            <li class="navbar__li">
+              <a class="navbar__a" href="#">
+                ABOUT
+              </a>
+            </li>
+            <li class="navbar__li">
+              <a class="navbar__a" href="#">
+                PROYECTOS
+              </a>
+            </li>
+            <li class="navbar__li">
+              <a class="navbar__a" href="#">
+                CONTACTO
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="navbar__section--3">
+          <i class="fa-brands fa-github-square"></i>
+          <i class="fa-brands fa-linkedin"></i>
+          <i class="fa-brands fa-whatsapp-square"></i>
+          <i class="fa-solid fa-cart-shopping"></i>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
